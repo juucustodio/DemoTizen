5bc99fe1aece84bc0d9748d65b0068d07d473f9f
Juliano Custódio <juliano.custodio@hotmail.com.br>
2017-08-24 13:45:30 -0300

    First Commit

---
+++ TizenApp/TizenApp/TizenApp.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace TizenApp
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            MainPage = new Obrigado();
+            
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
